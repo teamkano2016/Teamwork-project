b9e2e041be72e0df50880c0289eab83a6b051d40
milDob <mil.dob@yandex.com>
2016-06-23 20:25:53 +0300

    Testing

--- OOPGame/OOPGame/GameInterfaces/ITransformable.cs
+++ OOPGame/OOPGame/GameInterfaces/ITransformable.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace OOPGame
+﻿namespace OOPGame
 {
-    public interface ITransformable
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+
+	public interface ITransformable
     {
         // Move current hero in random position of the field.
         void Teleport();
